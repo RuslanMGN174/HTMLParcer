6dd5db80c3e58e4fd46711e0be87332a5c96389c
Vegas <rusland174@gmail.com>
2019-08-07 11:12:27 +0500

    final version #1

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -6,7 +6,6 @@ import org.jsoup.select.Elements;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
@@ -15,50 +14,38 @@ public class Loader {
     private static final String FILE_PATTERN = "([^s]+(.(jpg|png|gif|bmp))$)";
     private static String url;
     private static File file;
-
-//    "([^s]+(.(jpg|png|gif|bmp))$)"
+    private static BufferedImage image;
 
     public static void main(String[] args) throws Exception {
 
         url = "https://lenta.ru/";
 
         Document document = Jsoup.connect(url).get();
-
         Elements element = document.select("img");
-//        if (element.hasAttr("alt")){
-        element.forEach(System.out::println);
-//        }
 
-        BufferedImage image;
+        for (Element elements : element) {
+            String imageURL = elements.absUrl("src");
+            String imageName = getFileName(imageURL);
 
-//        for (Element elements : element) {
-//            String imageURL = elements.absUrl("src");
-//            System.out.println(imageURL);
-//
-//            String imageName = getFileName(imageURL);
-//            image = ImageIO.read(new URL(imageURL));
-//
-//            file = new File("C:\Users\knyazev.r\Desktop\New\\" + imageName);
-//            if (image != null)
-//                try {
-//                    ImageIO.write(image, "jpg", file);
-//                } catch (Exception e) {
-//                    continue;
-//                }
-//            System.out.println(imageName);
-//        }
+            if (isImageFile(imageName)) {
+                image = ImageIO.read(new URL(imageURL));
+            }
+
+            file = new File("C:\\Users\\knyazev.r\\Desktop\\New\\" + imageName);
+            ImageIO.write(image, "jpg", file);
+        }
     }
 
-    public static String setFileResolution(String name) {
+    protected static String setFileResolution(String name) {
         String suffix = ".png";
         return name + suffix;
     }
 
-    public static String getFileName(String name) {
-        String fullFileName = name.substring(name.lastIndexOf("/") + 1);
-        if (!Pattern.compile(FILE_PATTERN).matcher(fullFileName).matches()) {
-            return setFileResolution(fullFileName);
-        }
-        return fullFileName;
+    protected static String getFileName(String name) {
+        return name.substring(name.lastIndexOf("/") + 1);
+    }
+
+    private static boolean isImageFile(String file) {
+        return Pattern.compile(FILE_PATTERN).matcher(file).matches();
     }
 }
